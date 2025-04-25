cf995b922714f7838828b2de99fdf3f86b80c12e
Aryan Neupane <official.aryanneupane@gmail.com>
2025-04-25 23:26:26 +0545

    starting the createTask

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import{ AuthContext} from '../../context/AuthProvider'
 import { useContext } from 'react'
-function AllTask() {
+function AllTask({elem}) {
 
   const authData = useContext(AuthContext)
 
-
-
   return (
     <div  className='mt-10 '>
-        <div className="bg-[#2c2c2e] py-3 px-5 rounded-lg flex flex-col sm:flex-row justify-between gap-3 shadow-md mb-4">
+        <div className="bg-black py-3 px-5 rounded-lg flex flex-col sm:flex-row justify-between gap-3 shadow-md mb-4">
           <h2 className="font-semibold text-white">Employee Name</h2>
           <h3 className="font-semibold text-white">NewTask</h3>
           <h3 className="font-semibold text-white">Active Task</h3>
@@ -22,10 +20,10 @@ function AllTask() {
       {authData.employees.map((elem)=>{
           return <div className="bg-[#2c2c2e] py-3 px-5 rounded-lg flex flex-col sm:flex-row justify-between gap-3 shadow-md">
           <h2 className="font-semibold text-white">{elem.name}</h2>
-          <h3 className="text-blue-300">gfds</h3>
-          <h3 className="text-orange-300">gfds</h3>
-          <h3 className="text-green-300">gfds</h3>
-          <h3 className="text-red-300">gfds</h3>
+          <h3 className="text-blue-300">{elem.taskStats.newTask}</h3>
+          <h3 className="text-orange-300">{elem.taskStats.active}</h3>
+          <h3 className="text-green-300">{elem.taskStats.completed}</h3>
+          <h3 className="text-red-300">{elem.taskStats.failed}</h3>
          
         </div>
       })}
